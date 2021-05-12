e3b60407deaa439697dc17f80aa1f3839f28d53c
piyushwani004 <piyushwani04@gmail.com.com>
2021-05-12 12:12:42 +0530

    Project Files

--- src/main/java/com/example/demo/service/EmployeeServiceImple.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImple.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,29 @@ public class EmployeeServiceImple implements EmployeeService {
 	@Override
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
+	}
+
+	@Override
+	public void saveEmployee(Employee employee) {
+		this.employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee getEmployeeById(Long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		if (optional.isPresent()) {
+			employee = optional.get();
+		} else {
+			throw new RuntimeException("Employee Not Found for id :" + id);
+		}
+		return employee;
+	}
+
+	@Override
+	public void deleteEmployeeById(Long id) {
+		this.employeeRepository.deleteById(id);
+
 	}
 
 }
